4bc00d1c7ab64bf4a760483f377670666573464e
Lucas Nascimento <lucassnascimento20@gmail.com>
2020-11-17 23:55:48 -0300

    feat: ensure AxiosHttpClient calls axios with correct URL

---
+++ src/infra/axios-http-client/axios-http-client.ts
@@ -0,0 +1,8 @@
+import { httpPostParams } from '@/data/protocols/http';
+import axios from 'axios'
+
+export class AxiosHttpClient {
+  async post(params: httpPostParams<any>): Promise<void> {
+    await axios(params.url)
+  }
+}
