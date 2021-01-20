10a0045b6d55e4d744acc3f0b15f36d27bff855e
Lucas Nascimento <lucassnascimento20@gmail.com>
2021-01-19 23:00:43 -0300

    feat: add app router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Login } from '@/presentation/pages'
-
-ReactDOM.render(<Login />, document.getElementById('main'))
+import { Router } from '@/presentation/components'
+import '@/presentation/styles/global.scss'
+ReactDOM.render(<Router />, document.getElementById('main'))
